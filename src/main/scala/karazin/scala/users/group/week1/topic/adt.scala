8a2653d8b123c588db24c88319c3c9ee1914b00a
Igor Wolkov <igor.wolkov@wolkov.consulting>
2024-04-01 12:40:21 +0300

    Week 1 - Code comments updated

--- src/main/scala/karazin/scala/users/group/week1/topic/adt.scala
+++ src/main/scala/karazin/scala/users/group/week1/topic/adt.scala
@@ -1,26 +1,55 @@
 package karazin.scala.users.group.week1.topic
 
-object adt:
 
+object adt:
+  
+  /** Represents optional values. Instances of `Option`
+   * are either an instance of `Some` or the object `None`.
+  */
   enum Option[+V]:
 
+    /** Case `Some[A]` represents existing values of type
+     * `A`.
+     */
     case Some(x: V) extends Option[V]
 
+    /** This case object represents non-existent values.
+     */
     case None extends Option[Nothing]
 
-    // The method is used for defining execution pipelines
+    /** Returns the result of applying `f`` to this `Option`'s value if
+     * this `Option` is nonempty.
+     * Returns `None` if this `Option` is empty.
+     * Slightly different from `map` in that $f is expected to
+     * return an `Option` (which could be `None`).
+     *
+     *  @param f the function to apply
+     */
     def flatMap[Q](f: V => Option[Q]): Option[Q] =
       this match
         case Option.None     => Option.None
         case Option.Some(v)  => f(v)
 
-    // The method is used for changing the internal object
+    /** Returns a `Some` containing the result of applying `f`` to this `Option`'s
+     * value if this `Option` is nonempty.
+     * Otherwise return `None`.
+     *
+     *  @note This is similar to `flatMap` except here,
+     *  $f does not need to wrap its result in an `Option`.
+     *
+     *  @param f the function to apply
+     */
     def map[Q](f: V => Q): Option[Q] =
       this match
         case Option.None     => Option.None
         case Option.Some(v)  => Option.Some(f(v))
 
-  // Companion object to define constructor
   object Option:
+    /** An Option factory which creates Some(x) if the argument is not null,
+     * and None if it is null.
+     *
+     * @param x the value
+     * @return Some(value) if value != null, None if value == null
+     */
     def apply[V](v: V): Option[V] =
       if v == null then Option.None else Option.Some(v)
