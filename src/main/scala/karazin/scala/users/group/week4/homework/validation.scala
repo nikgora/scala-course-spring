a503092da63255c6465ea85e314334411410928c
mykolagora <nik.gora.2004@gmail.com>
2024-06-11 14:02:13 +0200

    week-4 - implementation of week-4

---
+++ src/main/scala/karazin/scala/users/group/week4/homework/validation.scala
@@ -0,0 +1,34 @@
+package karazin.scala.users.group.week4.homework
+
+import cats.data.ValidatedNec
+import cats.syntax.all.*
+import karazin.scala.users.group.week4.homework.errors.{DomainError, PasswordDoesNotMeetCriteria, UsernameHasSpecialCharacters, InvalidEmailFormat, AgeNotInRange, UsernameLengthInvalid}
+import karazin.scala.users.group.week4.homework.model.RegistrationData
+
+object validation:
+
+  type ValidationResult[A] = ValidatedNec[DomainError, A]
+
+  def validateUsername(username: String): ValidationResult[String] =
+    if username.matches("^[a-zA-Z0-9]+$") then
+      if username.length > 4 && username.length < 65 then username.validNec
+      else UsernameLengthInvalid.invalidNec
+    else UsernameHasSpecialCharacters.invalidNec
+
+  def validatePassword(password: String): ValidationResult[String] =
+    if password.matches("(?=^.{10,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$") then password.validNec
+    else PasswordDoesNotMeetCriteria.invalidNec
+
+  def validateEmail(email: String): ValidationResult[String] =
+    if email.matches("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$") then email.validNec
+    else InvalidEmailFormat.invalidNec
+
+  def validateAge(age: Int): ValidationResult[Int] =
+    if age >= 13 && age <= 100 then age.validNec
+    else AgeNotInRange.invalidNec
+
+  def validateRegistrationForm(username: String, password: String, email: String, age: Int): Either[List[DomainError], RegistrationData] =
+    (validateUsername(username),
+      validatePassword(password),
+      validateEmail(email),
+      validateAge(age)).mapN(RegistrationData.apply).toEither.leftMap(_.toList)
